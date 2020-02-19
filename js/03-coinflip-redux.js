326ea80028edac1aea9b52cdb85c393bc9811900
dahwindelacruz <dahwin.delacruz@gmail.com>
2020-02-19 11:25:03 -0800

    updated

--- js/03-coinflip-redux.js
+++ js/03-coinflip-redux.js
@@ -1,4 +1,10 @@
 let coinFlip;
-for (let i = 1; i <= 10; i += 1) {
-    coinFlip = Math.round(Math.random(i));
+let i;
+for (i = 1; i <= 10; i += 1) {
+    coinFlip = Math.round(Math.random(coinFlip));
+    window.console.log(coinFlip);
+} if (coinFlip === 0) {
+    window.console.log("Heads");
+} else {
+    window.console.log("Tails");
 }
